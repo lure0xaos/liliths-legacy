bd36591bebc7267dbc3c02094d7863c99c9defc8
Innoxia <innoxia7@gmail.com>
2024-11-27 20:24:47 +0000

    added patch notes, added behaviour for if Leg setType() needed to change LegConfiguration with a null owner

--- src/com/lilithsthrone/game/character/body/Leg.java
+++ src/com/lilithsthrone/game/character/body/Leg.java
@@ -167,6 +167,13 @@ public class Leg implements BodyPartInterface {
 					this.getType().applyLegConfigurationTransformation(owner, RacialBody.valueOfRace(type.getRace()).getLegConfiguration(), true, true);
 				}
 				owner.postTransformationCalculation();
+				
+			} else {
+				if(Main.game.isStarted() && !type.isLegConfigurationAvailable(this.getLegConfiguration())) {
+					System.err.println("Leg.setType() was passed a null owner, and the type '"+type.getTransformName()+"' requires conversion of LegConfiguration from "
+							+ "'"+this.getLegConfiguration().getName()+"' to '"+RacialBody.valueOfRace(type.getRace()).getLegConfiguration().getName()+"'.");
+					this.setLegConfigurationForced(type, RacialBody.valueOfRace(type.getRace()).getLegConfiguration());
+				}
 			}
 			return "";
 		}
